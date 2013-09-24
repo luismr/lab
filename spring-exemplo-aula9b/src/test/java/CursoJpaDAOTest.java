2c7104da158491d4858e9c6db7fd354c2516a5d5
Luis Machado Reis <luis.reis@singularideas.com.br>
2013-09-24 10:07:13 -0300

    Exemplo da Aula9 com Spring + JPA (Parcial)

---
+++ spring-exemplo-aula9b/src/test/java/CursoJpaDAOTest.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import wifi.model.dao.DAO;
+import wifi.model.data.Curso;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"/applicationContext.xml"})
+public class CursoJpaDAOTest {
+
+	@Autowired
+	private DAO<Curso> dao;
+
+	@Test
+	@Transactional
+	public void listAllTest() {
+		List<Curso> cursos = dao.listAll();
+		assertNotNull(cursos);
+		assertTrue(cursos.size() > 0);
+		
+		System.out.println(cursos);
+	}
+
+}
