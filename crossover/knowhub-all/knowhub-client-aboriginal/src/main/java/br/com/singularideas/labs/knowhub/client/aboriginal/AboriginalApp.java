d31cc451abf267270db52d02b40535b1c516e256
Luis Machado Reis <luis.reis@aurea.com>
2016-01-11 18:43:16 -0200

    client bugfixes

--- crossover/knowhub-all/knowhub-client-aboriginal/src/main/java/br/com/singularideas/labs/knowhub/client/aboriginal/AboriginalApp.java
+++ crossover/knowhub-all/knowhub-client-aboriginal/src/main/java/br/com/singularideas/labs/knowhub/client/aboriginal/AboriginalApp.java
@@ -3,6 +3,7 @@ package br.com.singularideas.labs.knowhub.client.aboriginal;
 import java.awt.EventQueue;
 
 import br.com.singularideas.labs.knowhub.client.aboriginal.gui.FormLogin;
+import br.com.singularideas.labs.knowhub.client.aboriginal.gui.util.ApplicationContextLoader;
 import br.com.singularideas.labs.knowhub.client.aboriginal.gui.util.DesktopUtil;
 import br.com.singularideas.labs.knowhub.client.aboriginal.gui.util.SingleInstanceThread;
 import br.com.singularideas.labs.knowhub.client.api.LoginService;
@@ -11,13 +12,13 @@ public abstract class AboriginalApp {
 
 	public static void main(String[] args) {
 		final String baseurl = detectBaseUrl(args);
-		
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					if (! SingleInstanceThread.hasAnInstancesRunning()) {
-						FormLogin app = new FormLogin(baseurl);
-						assert app != null;
+					if (!SingleInstanceThread.hasAnInstancesRunning()) {
+						registerShutdownHook();
+						new FormLogin(baseurl);
 					} else {
 						throw new AboriginalException("There is already an instance running!");
 					}
@@ -30,12 +31,26 @@ public abstract class AboriginalApp {
 
 	private static String detectBaseUrl(String[] args) {
 		String baseurl = LoginService.DEFAULT_BASEURL;
-		
+
 		if (args != null && args.length > 0) {
 			baseurl = args[0];
 		}
-		
+
 		return baseurl;
+	}
+
+	private static void registerShutdownHook() {
+		System.out.println("register a shutdown hook to clean up ... ");
+		
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				System.out.println("wait while we clean all mess that we did here ...");
+				ApplicationContextLoader.getInstance().shutdown();
+			}
+		});
+
+		System.out.println("done!");
 	}
 
 }
