490d41fa6a0e78fd3f31058e252b36113d27b5d3
Luis Machado Reis <luis.reis@aurea.com>
2017-01-15 17:42:23 -0200

    more tests

---
+++ source-code/spring-boot/gs-accessing-facebook/complete/src/main/java/hello/HelloController.java
@@ -0,0 +1,39 @@
+package hello;
+
+import javax.inject.Inject;
+
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.PagedList;
+import org.springframework.social.facebook.api.Post;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+
+    private Facebook facebook;
+	private ConnectionRepository connectionRepository;
+
+    @Inject
+    public HelloController(Facebook facebook, ConnectionRepository connectionRepository) {
+        this.facebook = facebook;
+		this.connectionRepository = connectionRepository;
+    }
+
+    @RequestMapping(method=RequestMethod.GET)
+    public String helloFacebook(Model model) {
+        if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
+            return "redirect:/connect/facebook";
+        }
+
+        model.addAttribute("facebookProfile", facebook.userOperations().getUserProfile());
+        PagedList<Post> feed = facebook.feedOperations().getFeed();
+        model.addAttribute("feed", feed);
+        return "hello";
+    }
+
+}
